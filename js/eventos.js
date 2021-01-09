727c92321adf72737581c95247bcc5affd4a847a
rimora <rimora7@hotmail.com>
2021-01-09 01:54:33 -0600

    eventos1

--- js/eventos.js
+++ js/eventos.js
@@ -1,19 +1,3 @@
-var json = {"alumnoUTP":[{"nombre":"Ricardo","apePaterno":"Carpio","edad":39},{"nombre":"Thiago","apePaterno":"Carpio","edad":5},{"nombre":"José","apePaterno":"Carpio","edad":74}]};
-
-function cargarDatos(){
-    var DatosJson = JSON.parse(JSON.stringify(json));
-    console.log(DatosJson.alumnoUTP.length);
-    $("#tabla").append('<tr><td>Nombre</td>'+
- 	'<td>Apellido paterno</td>' + 
- 	'<td>Edad</td>');
-    for (i = 0; i < DatosJson.alumnoUTP.length; i++){
- 
- $("#tabla").append('<tr>' + 
- 	'<td align="center" style="dislay: none;">' + DatosJson.alumnoUTP[i].nombre + '</td>'+
- 	'<td align="center" style="dislay: none;">' + DatosJson.alumnoUTP[i].apePaterno + '</td>'+
- 	'<td align="center" style="dislay: none;">' + DatosJson.alumnoUTP[i].edad + '</td>'+'</tr>');
-    }
-}
 $(document).ready(function() {
     
 /*
